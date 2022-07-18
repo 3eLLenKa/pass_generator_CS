183bd4dc27abb1d429140c9f975729c888af9b3f
alexk <alexk@DESKTOP-HCK9T1F>
2022-07-19 01:32:15 +0200

    Добавьте файлы проекта.

---
+++ PassGenerator2/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace PassGenerator2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.generate_button = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label1.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pass Generator";
+            // 
+            // generate_button
+            // 
+            this.generate_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.generate_button.Font = new System.Drawing.Font("Kristen ITC", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generate_button.Location = new System.Drawing.Point(98, 155);
+            this.generate_button.Name = "generate_button";
+            this.generate_button.Size = new System.Drawing.Size(105, 26);
+            this.generate_button.TabIndex = 1;
+            this.generate_button.Text = "Generate\r\n\r\n";
+            this.generate_button.UseVisualStyleBackColor = false;
+            this.generate_button.Click += new System.EventHandler(this.generate_button_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(26, 209);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Cyan;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(102, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Длина пароля";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "5",
+            "10",
+            "15",
+            "20"});
+            this.comboBox1.Location = new System.Drawing.Point(91, 73);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Cyan;
+            this.checkBox1.Location = new System.Drawing.Point(41, 102);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(221, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Использовать буквы и спец. символы";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PassGenerator2.Properties.Resources.gloria_rodgers_galaxy_signed;
+            this.ClientSize = new System.Drawing.Size(296, 285);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.generate_button);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button generate_button;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ PassGenerator2/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PassGenerator2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Pass generator (by 3eLLenKa)";
+
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+
+            this.textBox1.ReadOnly = true;
+        }
+
+        private bool check_symbols(char symbols)
+        {
+            if (symbols == '/' || symbols == '\\' ||
+                        symbols == '(' || symbols == ')' ||
+                        symbols == '{' || symbols == '}' ||
+                        symbols == '.' || symbols == ',' ||
+                        symbols == '\'' || symbols == ':')
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        private void generate_button_Click(object sender, EventArgs e)
+        {
+            string digits;
+            char symbols;
+            int size = Convert.ToInt32(comboBox1.Text);
+            bool check = checkBox1.Checked;
+
+            Random rnd = new Random();
+
+            textBox1.Text = "";
+
+            if (check == true)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    symbols = Convert.ToChar(rnd.Next(33, 128));
+
+                    if (check_symbols(symbols))
+                    {
+                        continue;
+                    }
+                    
+                    textBox1.Text += symbols;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    digits = rnd.Next(0, 9).ToString();
+                    textBox1.Text += digits;
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
